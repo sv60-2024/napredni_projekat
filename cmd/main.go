d9bdcf890c876ffcb2f6f5205c52efc18f4dbd5f
sv60-2024 <andrejpetrovic1789@gmail.com>
2026-08-20 18:59:39 +0200

    Implement external configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"napredni_algoritmi_projekat/internal/config"
+)
 
 func main() {
-	fmt.Println("KV Engine")
+	cfg, err := config.LoadConfig("config/config.json")
+	if err != nil {
+		fmt.Println("Greska:", err)
+		return
+	}
+
+	fmt.Println("Konfiguracija uspesno ucitana:")
+	fmt.Println("Memtable size:", cfg.MemtableSize)
+	fmt.Println("WAL segment size:", cfg.WALSegmentSize)
+	fmt.Println("Block size:", cfg.BlockSize)
+	fmt.Println("Block cache size:", cfg.BlockCacheSize)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,65 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	MemtableSize   int `json:"memtable_size"`
 	WALSegmentSize int `json:"wal_segment_size"`
 	BlockSize      int `json:"block_size"`
 	BlockCacheSize int `json:"block_cache_size"`
+}
+
+func DefaultConfig() Config {
+	return Config{
+		MemtableSize:   10,
+		WALSegmentSize: 10,
+		BlockSize:      4096,
+		BlockCacheSize: 20,
+	}
+}
+
+func LoadConfig(path string) (Config, error) {
+	cfg := DefaultConfig()
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return cfg, fmt.Errorf("greska pri citanju konfiguracije: %w", err)
+	}
+
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return cfg, fmt.Errorf("greska pri parsiranju konfiguracije: %w", err)
+	}
+
+	if err := validateConfig(cfg); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func validateConfig(cfg Config) error {
+	if cfg.MemtableSize <= 0 {
+		return fmt.Errorf("memtable_size mora biti veci od 0")
+	}
+
+	if cfg.WALSegmentSize <= 0 {
+		return fmt.Errorf("wal_segment_size mora biti veci od 0")
+	}
+
+	if cfg.BlockCacheSize <= 0 {
+		return fmt.Errorf("block_cache_size mora biti veci od 0")
+	}
+
+	if cfg.BlockSize != 4096 &&
+		cfg.BlockSize != 8192 &&
+		cfg.BlockSize != 16384 {
+		return fmt.Errorf("block_size mora biti 4096, 8192 ili 16384")
+	}
+
+	return nil
 }
